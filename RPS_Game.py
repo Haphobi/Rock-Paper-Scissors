d9ea593813b5fedfb97a78c728df27476573d19b
Haphobi <97302357+Haphobi@users.noreply.github.com>
2022-06-04 20:46:40 +0300

    Update RPS_Game.py

--- RPS_Game.py
+++ RPS_Game.py
@@ -1,74 +1,82 @@
-import random
-
-def RPSvalue():
-
- RPSvalue = random.ranint(1,9)
- return RPSvalue
+import random #Imports a randomiser module
+import time #Imports a module to add a pause
 
 # Infinite loop until broken
 while True:
+
+    # Defining inputs for yes or no
+    yes = ["y", "yes"]
+    no = ["n", "no"]
+
     print("Let's play Rock Paper Scissors!")
-    
+    time.sleep(1)
+
     # Prompts the user to pick between "rock", "paper" or "scissors"
-    choice = input("Choose from Rock, Paper, Scissors: ")
+    choice = input("""\n---CHOICE---
+Choose from Rock, Paper or Scissors
+>>> """)
+    print("------------\n")
     choice = choice.lower()
 
-    # Picks a random int
-    options = [1, 2, 3]
-    rngChoice = random.choice(options)
-    
-    if choice in ('rock', 'paper', 'scissors'):
+    #checks if choice is "rock" or "paper" or "scissors"
+    if choice in ("rock", "paper", "scissors"):
 
-        # Rock
-        if choice == 'rock':
-            
-            # Defines the outcome of the game based on rngChoice random int
-            dict = {
-            1: "> I choose Paper! Aw no I lose!",
-            2: "> I choose Rock! Welp, it's a tie!",
-            3: "> I choose Scissors! I win!",
-            }
+        possible_actions = ["rock", "paper", "scissors"]
+        # Picks a random choice for the computer
+        computer_action = random.choice(possible_actions)
 
-            # Prints dict (the outcome)
-            print(dict[rngChoice])
+        print(f"You chose {choice}, computer chose {computer_action}.")
+        time.sleep(0.5)
+        print("Calculating...")
+        time.sleep(1.5)
 
 
-        # Paper
-        if choice == 'paper':
-            
-            # Defines the outcome of the game based on rngChoice random int            
-            dict = {
-            1: "> I choose Scissors! Aw no I lose!",
-            2: "> I choose Paper! Welp, it's a tie!",
-            3: "> I choose Rock! I win!",
-            }
-
-            # Prints dict (the outcome)
-            print(dict[rngChoice])
-
-
-        # Scissors
-        if choice == 'scissors':
-
-            # Defines the outcome of the game based on rngChoice random int
-            dict = {
-            1: "> I choose Rock! Aw no I lose!",
-            2: "> I choose Scissors! Welp, it's a tie!",
-            3: "> I choose Paper! I win!",
-            }
-
-            # Prints dict (the outcome)
-            print(dict[rngChoice])
+        # tie
+        if choice == computer_action:
+            print("It's a tie!")
+        # rock
+        elif choice == "rock":
+            if computer_action == "scissors":
+                print("Rock smashes scissors! You win!")
+            else:
+                print("Paper covers rock! You lose.")
+        # paper
+        elif choice == "paper":
+            if computer_action == "rock":
+                print("Paper covers rock! You win!")
+            else:
+                print("Scissors cuts paper! You lose.")
+        # scissors
+        elif choice == "scissors":
+            if computer_action == "paper":
+                print("Scissors cuts paper! You win!")
+            else:
+                print("Rock smashes scissors! You lose.")
+        time.sleep(2)
 
         # Prompts the user to play again
-        newGame = input("Do you want to play again? (Y/N): ")
-        newGame = newGame.lower()
-        if newGame == "n":
+        new_game = input("""---CHOICE---
+        Do you want to play again? (Y/N): 
+        >>> """)
+        print("------------")
+        new_game = new_game.lower()
+        if new_game == "n":
             break
 
     #If the user has input an invalid choice, prompts to try again
     else:
-        invalidInput = input('Invalid input, do you want to try again? (Y/N): ')
-        invalidInput = invalidInput.lower()
-        if invalidInput == 'n':
+        yes_no = [yes, no]
+
+        choice = input('''---YES/NO---
+Invalid input, do you want to try again? (Y/N)
+>>> ''')
+        print('------------\n')
+        choice = choice.lower()
+        if choice in no:
             break
+        elif choice in yes:
+            pass
+        else:
+            print("Input not understood, breaking loop.")
+            break
+           
